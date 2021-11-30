1cb445d7ea20816c90d3a1e4de13cdc7e6adcafa
bbindae <bbindae@hotmail.com>
2021-11-29 23:58:33 -0800

    adding threshold

--- ink-level-detector.py
+++ ink-level-detector.py
@@ -9,9 +9,9 @@ def standardize_image(cv, imagePath, show_image):
 
     '''
     # Step 1: convert image jgp to png
-    img = cv.imread("./test.jpg")
-    cv.imwrite("./test.png", img)
-    png_img = cv.imread("./test.png")
+    img = cv.imread("./test2.jpg")
+    cv.imwrite("./test2.png", img)
+    png_img = cv.imread("./test2.png")
 
     # Step 2: resize an image
     width = 400
@@ -24,7 +24,12 @@ def standardize_image(cv, imagePath, show_image):
         cv.imshow("Resized PNG Image", resized_img)
         cv.waitKey(0)
     
-    
+    # Step 3: convert png to grayscale image
+    gray_img = cv.cvtColor(resized_img, cv.COLOR_BGR2GRAY)
+    if show_image:
+        cv.imshow("GrayScale image", gray_img)
+        cv.waitKey(0)
+      
     # Step 2: chnage it to grayscale and resize image
     # img = cv.imread("./test.png")
     
@@ -38,10 +43,27 @@ def standardize_image(cv, imagePath, show_image):
     # gray_img = cv.cvtColor(resized_img, cv.COLOR_BGR2GRAY)
     # cv.imwrite("./test_resized.png", gray_img)
     
-    return resized_img
+    return cv.split(gray_img)[0]
 
-def blur_image(cv, img):
-    img = cv.GaussianBlur()
+def blur_image(cv, img, show_image):
+    img = cv.GaussianBlur(img, (7, 7), 0)
+    if show_image:
+        cv.imshow("Gray smoothed 7 x 7", img)
+        cv.waitKey(0)
+
+    return img
+
+def get_threshold(cv, plt, img, show_image):
+    if show_image:
+        plt.hist(img.ravel(), 256, [0, 256])
+        plt.show()
+    
+    (T, segmented_img) = cv.threshold(img, 0, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)
+    if show_image:
+        cv.imshow("segmented image", segmented_img)
+        print("Threshold: {}".format(T))
+        cv.waitKey(0)
+
 # Step 1: convert an image to an grayscale image
 # def convert_to_grayscale(cv, imagePath):
 #     '''
@@ -52,8 +74,9 @@ def blur_image(cv, img):
 #     '''
 
 print("convert images")
-standardize_image(cv, "hi", True)
-
+img = standardize_image(cv, "hi", True)
+img = blur_image(cv, img, True)
+get_threshold(cv, plt, img, True)
 
 # pen_image = cv.imread("./images/pen1.png")
 # pen_gray = cv.split(pen_image)[0]
